5394fdace5fac989dd103ede3896a9a31f84102f
u3u <quq@quq.cat>
2017-07-10 23:09:11 +0800

    配置文件

--- src/utils/config.ts
+++ src/utils/config.ts
@@ -1,5 +1,10 @@
 export default {
-  api: {
-    playlist: 'https://api.quq.cat/user/playlist?uid=273292534&limit=1'
+  baseUrl: 'https://api.quq.cat',
+  apis: {
+    search: '/search?keywords=Hanser',
+    playlist: '/user/playlist?uid=273292534&limit=1',
+    collect: '/playlist/detail?id=805272969',
+    playurl: '/music/url?id=433681435',
+    lyric: '/lyric?id=433681435'
   }
 }
