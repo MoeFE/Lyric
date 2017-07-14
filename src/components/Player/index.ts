490d35e71444c10b391dea0c162a9e16c7fc47e1
u3u <quq@quq.cat>
2017-07-14 14:26:01 +0800

    对接 vuex

--- src/components/Player/index.ts
+++ src/components/Player/index.ts
@@ -1,24 +1,49 @@
 import Vue from 'vue'
 import Component from 'vue-class-component'
 import WithRender from './index.html?style=./index.scss'
-
-import { Prop } from 'vue-property-decorator'
 import * as APlayer from 'aplayer'
+
+import { State } from 'store/modules/aplayer/state'
+import { Watch } from 'vue-property-decorator'
+import { Getter, Action, namespace } from 'vuex-class'
+const ModuleGetter = namespace('aplayer', Getter)
+const ModuleAction = namespace('aplayer', Action)
 
 @WithRender
 @Component
 export class Player extends Vue {
-  mounted () {
-    // tslint:disable-next-line:no-unused-expression
-    new APlayer({
-      element: this.$refs.aplayer,
-      listmaxheight: '115px',
-      music: {
-        title: 'あっちゅ～ま青春!',
-        author: '七森中☆ごらく部',
-        url: 'http://devtest.qiniudn.com/あっちゅ～ま青春!.mp3',
-        pic: 'http://devtest.qiniudn.com/あっちゅ～ま青春!.jpg'
-      }
-    })
+
+  /** APlayer 播放器选项 */
+  @ModuleGetter('options') options: () => State
+  /** [Action] 获取收藏歌单 */
+  @ModuleAction('favorites') favAction: () => void
+
+  /** APlayer 播放器实例 */
+  private aplayer = null
+
+  created () {
+    this.$store.watch(this.options, options => {
+      if (this.aplayer) this.aplayer.destroy()
+      this.aplayer = new APlayer({ ...options, element: this.$refs.aplayer })
+      this.aplayer.element.querySelector('.aplayer-icon-menu').click()
+      // 注册事件
+      this.aplayer.on('play', this.onplay)
+      this.aplayer.on('pause', this.onpause)
+      this.aplayer.on('canplay', this.oncanplay)
+      this.aplayer.on('playing', this.onplaying)
+      this.aplayer.on('ended', this.onended)
+      this.aplayer.on('error', this.onerror)
+    }, { deep: true })
+    setTimeout(() => this.favAction(), 1000)
   }
+
+  // tslint:disable:no-empty
+  onplay () {}
+  onpause () {}
+  oncanplay () {}
+  onplaying () {}
+  onended () {}
+  onerror () {}
+  // tslint:enable:no-empty
+
 }
