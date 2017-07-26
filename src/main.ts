90fe69a1f93d55908c5103ead56e67fc0104212f
u3u <quq@quq.cat>
2017-07-26 14:18:02 +0800

    删除控制台输出

--- src/main.ts
+++ src/main.ts
@@ -22,22 +22,5 @@ new Vue({
   el: 'app',
   router,
   store,
-  render: (h) => h(App),
-  beforeCreate () {
-    const time = { start: 0, end: 0, timespan: () => time.end - time.start }
-    time.start = Number(new Date())
-    const img = new Image()
-    img.onload = () => { // 确保文字必须在图片输出之后再输出
-      time.end = Number(new Date())
-      setTimeout(() => {
-        const moe = 'color: #fd5557; font-weight: bold'
-        console.log('\n%cMoe %cis justice!!!', moe, 'font-weight: bold')
-        console.log('%cqwq欢迎加入 %c@MoeFE Studio', 'font-weight: bold', moe)
-        console.log('%cGitHub: %chttps://github.com/MoeFE', 'font-weight: bold', 'color: #42b983; font-weight: bold')
-        console.log('')
-      }, time.timespan())
-    }
-    img.src = 'https://avatars3.githubusercontent.com/u/29977599?v=3&s=100'
-    console.img(img.src)
-  }
+  render: (h) => h(App)
 })
