be291531fe1b3aee6d71f2bb8d9a13c7a9a334bd
u3u <quq@quq.cat>
2017-07-26 14:58:19 +0800

    获取网易云歌单

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,12 +1,12 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import Index from 'views'
+import IndexPage from 'views'
 
 Vue.use(Router)
 
 export default new Router({
   mode: 'history',
   routes: [
-    { path: '/', name: 'Index', component: Index }
+    { path: '/', name: 'Index', component: IndexPage }
   ]
 })
--- src/views/index.ts
+++ src/views/index.ts
@@ -2,6 +2,8 @@ import Vue from 'vue'
 import Component from 'vue-class-component'
 import WithRender from './index.html?style=./index.scss'
 
+import { Getter, Action } from 'vuex-class'
+
 import { Editor, Footer as vFooter, Form as vForm, Help, Step, Upload } from 'components'
 import APlayer from 'vue-aplayer-plugin'
 import 'vue-aplayer-plugin/dist/APlayer.min.css'
@@ -9,4 +11,17 @@ Vue.use(APlayer)
 
 @WithRender
 @Component({ components: { Editor, vFooter, vForm, Help, Step, Upload } })
-export default class Main extends Vue { }
+export default class IndexPage extends Vue {
+
+  @Getter('aplayer/list')
+  private readonly music: Array<APlayer.Music>
+  @Action('aplayer/getMusics')
+  private getMusics: () => void
+  @Action('aplayer/getLyricAsync')
+  private getLyricAsync: (id: number) => void
+
+  private async created (): Promise<void> {
+    this.getMusics()
+  }
+
+}
