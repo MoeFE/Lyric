d478d02cb30cc1bbd45177def89a430cdf4494d9
u3u <quq@quq.cat>
2017-07-26 14:57:52 +0800

    修改接口地址

--- src/utils/config.ts
+++ src/utils/config.ts
@@ -2,8 +2,7 @@ export abstract class Config {
   public static baseURL: string = 'https://api.quq.cat'
   public static apis = {
     search: '/search?keywords=Hanser',
-    playlist: '/user/playlist?uid=273292534&limit=1',
-    collection: '/playlist/detail?id=805272969',
+    playlist: '/playlist/detail?id=805272969',
     playurl: '/music/url',
     lyric: '/lyric'
   }
