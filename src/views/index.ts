553ddc7d76c1490c8b8cf62f0a40569df3fdf16b
u3u <quq@quq.cat>
2017-07-26 14:19:46 +0800

    注册组件

--- src/views/index.ts
+++ src/views/index.ts
@@ -1,16 +1,12 @@
 import Vue from 'vue'
 import Component from 'vue-class-component'
 import WithRender from './index.html?style=./index.scss'
-import './global.scss'
 
-import { Prop } from 'vue-property-decorator'
-import { Help } from 'components/Help'
-import { Step } from 'components/Step'
-import { VForm } from 'components/Form'
-import { Upload } from 'components/Upload'
-import { VFooter } from 'components/Footer'
-import { Player as APlayer } from 'components/Player'
+import { Editor, Footer as vFooter, Form as vForm, Help, Step, Upload } from 'components'
+import APlayer from 'vue-aplayer-plugin'
+import 'vue-aplayer-plugin/dist/APlayer.min.css'
+Vue.use(APlayer)
 
 @WithRender
-@Component({ components: { Help, Step, VForm, Upload, APlayer, VFooter } })
+@Component({ components: { Editor, vFooter, vForm, Help, Step, Upload } })
 export default class Main extends Vue { }
