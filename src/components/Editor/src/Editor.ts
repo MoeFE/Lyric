4dcff5660088e65b2b284042240700c239d01fd1
u3u <quq@quq.cat>
2017-07-28 11:10:59 +0800

    添加 value 属性

--- src/components/Editor/src/Editor.ts
+++ src/components/Editor/src/Editor.ts
@@ -2,7 +2,14 @@ import Vue from 'vue'
 import Component from 'vue-class-component'
 import WithRender from './Editor.html?style=./Editor.scss'
 
+import { Prop } from 'vue-property-decorator'
+
 @WithRender
 @Component
-export class Editor extends Vue { }
+export class Editor extends Vue {
+
+  @Prop({ type: String, required: true })
+  public readonly value: string
+
+}
 export default Editor
