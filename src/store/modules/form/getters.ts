fd9b96d4239f56a983dace6e79691439e9c6f5d1
u3u <quq@quq.cat>
2017-07-28 11:36:46 +0800

    显示空字符串

--- src/store/modules/form/getters.ts
+++ src/store/modules/form/getters.ts
@@ -3,10 +3,10 @@ import { State } from './state'
 
 export const model = (state: State) => state.model
 export const meta = (state: State) => (`
-[ti:${state.model.songName}]
-[al:${state.model.singerName}]
-[ar:${state.model.albumName}]
-[by:${state.model.authorName}]
+[ti:${state.model.songName || ''}]
+[al:${state.model.singerName || ''}]
+[ar:${state.model.albumName || ''}]
+[by:${state.model.authorName || ''}]
 [t_time:(00:00)]`.trim() + '\n\n')
 
 export const getters = { model, meta } as GetterTree<State, any>
