b1e51fbdd6e1600a67cab2db5a9a82b53ca948de
u3u <quq@quq.cat>
2017-07-28 11:10:31 +0800

    添加 active 属性

--- src/components/Step/src/Step.ts
+++ src/components/Step/src/Step.ts
@@ -2,7 +2,14 @@ import Vue from 'vue'
 import Component from 'vue-class-component'
 import WithRender from './Step.html?style=./Step.scss'
 
+import { Prop } from 'vue-property-decorator'
+
 @WithRender
 @Component
-export class Step extends Vue { }
+export class Step extends Vue {
+
+  @Prop({ type: Number, default: 0, required: false })
+  private active: number
+
+}
 export default Step
