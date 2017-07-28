788abe2e8f1fcc048f0ae453e32db6c60625bbd7
u3u <quq@quq.cat>
2017-07-28 11:11:24 +0800

    绑定表单数据和表单验证

--- src/components/Form/src/Form.ts
+++ src/components/Form/src/Form.ts
@@ -3,20 +3,40 @@ import Component from 'vue-class-component'
 import WithRender from './Form.html'
 import './Form.scss'
 
+import { Watch } from 'vue-property-decorator'
+import { Mutation } from 'vuex-class'
+import { UPDATE_MODEL } from 'store/modules/form/types'
+
 @WithRender
 @Component
 export class Form extends Vue {
 
   /** 表单数据 */
-  private readonly model = {
+  private readonly model: Model = {
     songName: null, // 歌曲名
-    singerName: null // 歌手名
+    singerName: null, // 歌手名
+    albumName: null, // 专辑名
+    authorName: null // 编辑人
   }
 
   /** 验证规则 */
   private readonly rules = {
     songName: { required: true, message: '请输入歌曲名称' },
     singerName: { required: true, message: '请输入歌手名称，多个歌手之间用 / 或 & 隔开' }
+  }
+
+  @Mutation(UPDATE_MODEL, { namespace: 'form' })
+  private updateModel: (model: Model) => void
+
+  /** 搜索事件 */
+  private searchHandler (queryString: string, callback: (data: any[]) => void): void {
+    console.log('queryString: ', queryString)
+    console.log(callback)
+  }
+
+  @Watch('model', { deep: true })
+  private modelChange (): void {
+    this.updateModel(this.model)
   }
 
 }
