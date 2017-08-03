fda9c5adad2a78526931478af8e55f6bae6c7c87
u3u <quq@quq.cat>
2017-08-03 15:08:48 +0800

    新增编辑器基本功能

--- src/components/Editor/src/Editor.ts
+++ src/components/Editor/src/Editor.ts
@@ -3,6 +3,7 @@ import Component from 'vue-class-component'
 import WithRender from './Editor.html?style=./Editor.scss'
 
 import { Prop, Watch } from 'vue-property-decorator'
+import { LRCUtil } from 'utils'
 
 @WithRender
 @Component
@@ -12,10 +13,22 @@ export class Editor extends Vue {
   public readonly value: string
   @Prop({ type: Object, required: true, default: () => ({ singerName: '', songName: '', albumName: '', authorName: '' } as Model) })
   public readonly model: Model
+  @Prop({ type: Number, required: true, default: 0 })
+  public readonly currentTime: number
 
   private get meta (): string {
     const { songName, singerName, albumName, authorName } = this.model
     return `[ti:${songName}]\n[ar:${singerName}]\n[al:${albumName}]\n[by:${authorName}]\n\n`
+  }
+
+  private get textarea (): HTMLTextAreaElement {
+    const div = this.$refs.textarea['$el'] as HTMLDivElement
+    const textarea = div.firstChild as HTMLTextAreaElement
+    return textarea
+  }
+
+  private lineIndex (): number {
+    return this.value.substr(0, this.textarea.selectionStart).split('\n').length - 1
   }
 
   private updateValue (value: string): void {
@@ -28,31 +41,62 @@ export class Editor extends Vue {
   @Watch('model', { deep: true })
   private modelChange (): void {
     const regex: RegExp = /\[ti:.*\]|\[ar:.*\]|\[al:.*\]|\[by:.*\]/g
-    const resetValue = (value: string) => value.replace(regex, '').trim()
+    const resetValue = (value: string) => value.replace(regex, '').trim() + '\n'
     const value: string = resetValue(this.value)
     this.$emit('input', value ? this.meta + value : '')
   }
 
+  @Watch('currentTime')
+  private updateCurrentTime (): void {
+    LRCUtil.currentTime = this.currentTime
+  }
+
+  private timeTagAction (action: 'insertTimeTag' | 'replaceTimeTag' = 'insertTimeTag'): void {
+    const line: number = this.lineIndex()
+    const start: number = this.value.split('\n').filter((_, index) => index < line).join().length + 1
+    this.$emit('input', LRCUtil[action](this.value, line))
+    // 将光标移动到下一行并调整滚动条位置
+    this.$nextTick(() => {
+      this.textarea.selectionStart = this.textarea.selectionEnd = start + this.value.split('\n')[line].length + 1
+      this.textarea.scrollTop = Number.parseInt(window.getComputedStyle(this.textarea)['line-height']) * (this.lineIndex() - 7) // 滚动条位置优化
+      this.textarea.focus()
+    })
+  }
+
   private add (): void {
-    console.log('add')
+    this.timeTagAction('insertTimeTag')
   }
   private rp (): void {
-    console.log('rp')
+    this.timeTagAction('replaceTimeTag')
   }
   private rm (): void {
-    console.log('rm')
+    const line: number = this.lineIndex()
+    const start: number = this.value.split('\n').filter((_, index) => index < line).join().length + 1
+    this.$emit('input', LRCUtil.removeTimeTag(this.value, this.lineIndex()))
+    this.$nextTick(() => {
+      this.textarea.selectionStart = this.textarea.selectionEnd = start
+      this.textarea.focus()
+    })
   }
   private del (): void {
-    console.log('del')
+    this.$emit('input', LRCUtil.deleteTimeTag(this.value))
+    this.$nextTick(() => {
+      this.textarea.selectionStart = this.textarea.selectionEnd = this.meta.length
+      this.textarea.scrollTop = 0
+      this.textarea.focus()
+    })
   }
   private undo (): void {
-    console.log('undo')
+    document.execCommand('undo')
+    this.$nextTick(() => this.textarea.focus())
   }
   private redo (): void {
-    console.log('redo')
+    document.execCommand('redo')
+    this.$nextTick(() => this.textarea.focus())
   }
   private preview (): void {
-    console.log('preview')
+    this.$emit('preview')
+    this.$nextTick(() => this.textarea.focus())
   }
 
 }
