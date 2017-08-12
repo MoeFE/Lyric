31fcd286da0d5706a8b49fe3f79a0877f57a57e6
u3u <quq@quq.cat>
2017-08-12 13:28:13 +0800

    上传组件

--- src/components/Upload/src/Upload.ts
+++ src/components/Upload/src/Upload.ts
@@ -4,5 +4,16 @@ import WithRender from './Upload.html?style=./Upload.scss'
 
 @WithRender
 @Component
-export class Upload extends Vue { }
+export class Upload extends Vue {
+  private changeHandler (): void {
+    const target = event.target as HTMLInputElement
+    const file = target.files[0]
+    const reader = new FileReader()
+    target.value = null
+    reader.onload = () => {
+      this.$emit('upload', file, reader, target)
+    }
+    reader.readAsDataURL(file)
+  }
+}
 export default Upload
